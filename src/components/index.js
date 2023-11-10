ad7e6f7fbac5ff92e1206cf0f1c9ca80bc839bbb
Rohan Gope <rg2512@it.jgec.ac.in>
2023-11-10 21:23:47 +0530

    Add new components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,16 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
-
+import Input from "./Input";
+import Button from "./Button";
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Container,
     LogoutBtn,
-    Logo
-
+    Logo,
+    Input,
+    Button,
+    RTE
 }
